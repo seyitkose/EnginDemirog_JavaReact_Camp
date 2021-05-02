2b2a635e7fd1a6885967631fb10d9a24e875dd9e
seyitkose <62472005+seyitkose@users.noreply.github.com>
2021-05-02 13:08:19 +0300

    2.Day_HomeWork_1

---
+++ 2.Day_HomeWork_1/EducationInstructor.java
@@ -0,0 +1,12 @@
+package ders2_odev1;
+
+public class EducationInstructor {
+	
+	public void addToInstructor(Education education2) {
+		System.out.println("Seçilen Eğitmen: " + education2.educationInstructor  );
+		
+	}
+
+	
+
+}
